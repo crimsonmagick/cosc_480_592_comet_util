c3ecd4043a0c90c887b120024b92647af183949a
Welby Seely <welbyseely@gmail.com>
2025-04-12 19:34:01 -0400

    Takes a results-dir as a param

--- src/graph_results.py
+++ src/graph_results.py
@@ -1,9 +1,17 @@
+import argparse
 import csv
 import matplotlib.pyplot as plt
 
 
 def main():
-    print("hi")
+    parser = argparse.ArgumentParser(description="Graphs the results of CoMeT benchmarks.")
+    parser.add_argument(
+        '--results-dir',
+        type=str,
+        required=True,
+        help='Results to parse')
+    args = parser.parse_args()
+    print(f"results-dir={args.results_dir}")
     
 if __name__ == '__main__':
     main()
