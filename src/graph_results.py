4dd7fb95e4d3257334a104e37bfdbac1611f3296
Welby Seely <welbyseely@gmail.com>
2025-04-12 19:15:54 -0400

    Initial commit

---
+++ src/graph_results.py
@@ -0,0 +1,9 @@
+import csv
+import matplotlib.pyplot as plt
+
+
+def main():
+    print("hi")
+    
+if __name__ == '__main__':
+    main()
---
+++ test_runs/fft-dvfs-1-4/sim.scripts.py
@@ -0,0 +1,3 @@
+import sys
+sys.argv = [ "/home/welb/workspace/CoMeT/scripts/memTherm_core.py", "" ]
+execfile("/home/welb/workspace/CoMeT/scripts/memTherm_core.py")
